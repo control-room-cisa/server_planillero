6df45be456019db028533fa89a85b27dfe5cee04
iksermd@gmail.com <ikser.marquez@flxgrp.com>
2025-09-27 03:30:13 -0600

    Agregar campo de visibilidad a empresas genericas que solo se usan en jobs

---
+++ prisma/migrations/20250927041231_empresa_visible_boolean/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `empresas` ADD COLUMN `visible` BOOLEAN NULL DEFAULT false;
--- src/dtos/empresa.dto.ts
+++ src/dtos/empresa.dto.ts
@@ -1,21 +1,22 @@
 // src/dtos/empresa/empresaDtos.ts
 
 export interface DepartamentoDto {
-  id:          number;
-  nombre:      string | null;      // string (o Date según tu schema), no Date
-  codigo?:     string | null;
-  createdAt:   Date | null;
-  updatedAt?:  Date | null;
-  deletedAt?:  Date | null;
-  empresaId:   number;             // Prisma te lo está devolviendo
+  id: number;
+  nombre: string | null; // string (o Date según tu schema), no Date
+  codigo?: string | null;
+  createdAt: Date | null;
+  updatedAt?: Date | null;
+  deletedAt?: Date | null;
+  empresaId: number; // Prisma te lo está devolviendo
 }
 
 export interface EmpresaConDepartamentosDto {
-  id:           number;
-  codigo?:      string | null;
-  nombre?:      string | null;
-  createdAt:    Date | null;
-  updatedAt?:   Date | null;
-  deletedAt?:   Date | null;
+  id: number;
+  codigo?: string | null;
+  nombre?: string | null;
+  visible?: boolean | null;
+  createdAt: Date | null;
+  updatedAt?: Date | null;
+  deletedAt?: Date | null;
   departamentos: DepartamentoDto[];
 }
